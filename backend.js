cf7d9a0ead2cf4aba1f164f824a24dd4ea235420
Jonathan Warner <jaxbot@gmail.com>
2014-11-22 20:55:49 -0500

    Initial upload stub

--- backend.js
+++ backend.js
@@ -4,6 +4,7 @@ var url = require('url');
 
 var config = require('./config');
 var database = require('./database');
+var upload = require('./upload');
 
 database.init();
 
@@ -55,6 +56,12 @@ function handler (req, res) {
 							res.write("success");
 							res.end();
 						}
+					});
+				return;
+				case '/api/upload' :
+					upload.upload(body, function(err, file) {
+						res.write(file);
+						res.end();
 					});
 				return;
 			}
---
+++ upload.js
@@ -0,0 +1,10 @@
+exports.uploadFile = function (data, callback) {
+	var tmpfile = "./images/" + (new Date).getTime() + ".jpg";
+
+	fs.writeFile(tmpfile, data.substring(data.indexOf(",") + 1), "base64", function(err) {
+		data = null;
+
+		if (err) callback(err);
+		callback(null, tmpfile);
+	});
+}
